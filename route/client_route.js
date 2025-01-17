527da93949b5f4c006b59e1f8855159bc1ff4687
Dia505 <dia48261@gmail.com>
2025-01-17 08:48:07 +0545

    get client by id: role authorization changes

--- route/client_route.js
+++ route/client_route.js
@@ -1,24 +1,24 @@
 const express = require("express")
 const router = express.Router();
-const {findAll, save, findById, deleteById, update, updateProfilePicture} = require("../controller/client_controller");
+const { findAll, save, findById, deleteById, update, updateProfilePicture } = require("../controller/client_controller");
 const clientValidation = require("../validation/client_validation")
-const {authenticateToken} = require("../security/auth") 
-const {authorizeRole} = require("../security/auth");
+const { authenticateToken } = require("../security/auth")
+const { authorizeRole } = require("../security/auth");
 
 const multer = require("multer");
 const storage = multer.diskStorage({
-    destination: function(req, res, cb) {
+    destination: function (req, res, cb) {
         cb(null, "client_images")
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
 })
-const upload = multer({storage})
+const upload = multer({ storage })
 
 router.get("/", authenticateToken, authorizeRole("admin"), findAll);
 router.post("/", clientValidation, upload.single("profile_picture"), save);
-router.get("/:id", authenticateToken, authorizeRole("admin"), findById);
+router.get("/:id", authenticateToken, authorizeRole("admin", "client"), findById);
 router.delete("/:id", authenticateToken, authorizeRole("client"), deleteById);
 router.put("/:id", authenticateToken, authorizeRole("client"), update);
 router.put("/:id/profile-picture", authenticateToken, authorizeRole("client"), upload.single("profile_picture"), updateProfilePicture);
