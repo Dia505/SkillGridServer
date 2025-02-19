3e4a78d422761d56e308eb974c88c659263069e4
Dia505 <dia48261@gmail.com>
2025-02-20 00:54:33 +0545

    Client view and update

--- route/appointment_route.js
+++ route/appointment_route.js
@@ -1,18 +1,18 @@
 const express = require("express")
 const router = express.Router();
-const {findAll, save, findById, findByFreelancerServiceId, findByClientId, findByFreelancerId, deleteById, update, acceptAppointment} = require("../controller/appointment_controller");
+const { findAll, save, findById, findByFreelancerServiceId, findByClientId, findByFreelancerId, deleteById, update, acceptAppointment } = require("../controller/appointment_controller");
 const appointmentValidation = require("../validation/appointment_validation");
-const {authenticateToken} = require("../security/auth");
-const {authorizeRole} = require("../security/auth");
+const { authenticateToken } = require("../security/auth");
+const { authorizeRole } = require("../security/auth");
 
 router.get("/", authenticateToken, authorizeRole("admin"), findAll);
 router.post("/", appointmentValidation, authenticateToken, authorizeRole("client"), save);
 router.get("/:id", authenticateToken, findById);
-router.get("/freelancer-service/:freelancer_service_id", authenticateToken, authorizeRole("admin","freelancer"), findByFreelancerServiceId);
-router.get("/client/:client_id", authenticateToken, authorizeRole("admin","client"), findByClientId);
+router.get("/freelancer-service/:freelancer_service_id", authenticateToken, authorizeRole("admin", "freelancer"), findByFreelancerServiceId);
+router.get("/client/:client_id", authenticateToken, authorizeRole("admin", "client"), findByClientId);
 router.get("/freelancer/:freelancer_id", authenticateToken, findByFreelancerId);
 router.delete("/:id", authenticateToken, deleteById);
-router.put("/:id", authenticateToken, authorizeRole("client","freelancer"), update);
+router.put("/:id", authenticateToken, authorizeRole("client", "freelancer"), update);
 router.put("/accept/:id", authenticateToken, authorizeRole("freelancer"), acceptAppointment);
 
 module.exports = router;
